a5f1da2156264929d5e80630d41198a92e27435b
Luthfi  bustillos <luthfibustillos@Luthfis-Mac-mini.local>
2020-06-10 00:12:58 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # treasurer
+
+
+# Create virtual environmnent
+python3 -m venv .venv
+
+### activate env
+source .venv/bin/activate
+
+# Install requirements
+pip3 install -r requirements.txt
---
+++ treasurer/app.py
@@ -0,0 +1,17 @@
+# We do this so that we can take advantage of async code from gevent
+from gevent import monkey
+monkey.patch_all()
+
+import os
+
+import requests
+from flask import Flask, request
+
+api_port = os.environ['PORT_API']
+api_url = f'http://slow_api:{api_port}/'
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello there"
